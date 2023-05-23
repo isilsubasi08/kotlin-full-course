61ef09d246fed3046cc03387b70c968e96b05cbb
isilsubasi19 <isil.subasi19@gmail.com>
2023-05-23 12:04:11 +0300

    Variables

---
+++ HelloKotlin/src/main/kotlin/Main.kt
@@ -0,0 +1,43 @@
+fun main() {
+
+
+
+    val userName  = "AndroidZamani"
+    val age  = 28
+    println("Hello $userName your age is : $age")
+
+    val minIntegerValue=Int.MIN_VALUE
+    val maxIntegerValue=Int.MAX_VALUE
+
+    println("Int minumum value is : $minIntegerValue")
+    println("Int maximum value is : $maxIntegerValue")
+
+    val minByteValue=Byte.MIN_VALUE
+    val maxByteValue=Byte.MAX_VALUE
+
+    println("Byte minumum value is : $minByteValue")
+    println("Byte maximum value is : $maxByteValue")
+
+    val minShortValue = Short.MIN_VALUE
+    val maxShortValue = Short.MAX_VALUE
+
+    println("Byte minumum value is : $minShortValue")
+    println("Byte maximum value is : $maxShortValue")
+
+    val minLongValue = Long.MIN_VALUE
+    val maxLongValue = Long.MAX_VALUE
+
+    println("Long minumum value is : $minLongValue")
+    println("Long maximum value is : $maxLongValue")
+
+
+    val myFloatNumber=2.5f
+    val myDoubleNumber=2.5
+    println("myFloatNumber : $myFloatNumber")
+    println("myDoubleNumber : $myDoubleNumber")
+
+
+
+
+
+}
--- README.md
+++ README.md
@@ -2,3 +2,8 @@
 
 Kotlin, JetBrains firması tarafından geliştirilmiş, modern bir programlama dilidir. Java Virtual Machine (JVM) üzerinde çalışabilmesinin yanı sıra, Android uygulama geliştirmek için de kullanılabilmektedir. Google kaynaklarında Android geliştirmek için resmi dil olarak belirlenmiştir.
 
+## Variables/Değişkenler
+Kotlin programlama dilinin güçlü ve esnek bir özelliği, değişkenlerin etkili bir şekilde kullanılabilmesidir. Değişkenler, verileri saklamak ve işlemek için önemli bir rol oynar.
+Örnek kod ve anlatıma burdan ulaşabilirsiniz.<br>
+
+-  [Tık✨]()
